a4375763b461ef2324d7f9cfad9d5882aa880a27
Alisson <binho_alisson@hotmail.com>
2025-09-24 19:42:31 -0300

    first commit

---
+++ src/main/java/com/alisson/gas_station/GasStationApplication.java
@@ -0,0 +1,13 @@
+package com.alisson.gas_station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasStationApplication.class, args);
+	}
+
+}
