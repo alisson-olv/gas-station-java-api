139dcc52d99dc1dc0d6273dc334e26b083229010
Alisson <binho_alisson@hotmail.com>
2025-09-27 00:42:01 -0300

    feat: fix the mandatory properties from FuelType entity

--- src/main/java/com/alisson/gas_station/infrastructure/entities/FuelType.java
+++ src/main/java/com/alisson/gas_station/infrastructure/entities/FuelType.java
@@ -1,6 +1,9 @@
 package com.alisson.gas_station.infrastructure.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -18,9 +21,12 @@ public class FuelType {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
+    @NotBlank(message = "Fuel name cannot be empty/null")
     private String name;
 
-    @Column(name = "price_per_liter")
+    @Column(name = "price_per_liter", nullable = false)
+    @NotNull(message = "Price cannot be empty/null")
+    @Positive(message = "Price must be greater than zero")
     private BigDecimal pricePerLiter;
 }
