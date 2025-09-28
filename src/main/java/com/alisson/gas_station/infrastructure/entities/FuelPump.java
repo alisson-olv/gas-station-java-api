0d469fac62197c52039dd58b65994f2eac2d1317
Alisson <binho_alisson@hotmail.com>
2025-09-28 01:29:12 -0300

    feat: create validation to FuelPump Entity

--- src/main/java/com/alisson/gas_station/infrastructure/entities/FuelPump.java
+++ src/main/java/com/alisson/gas_station/infrastructure/entities/FuelPump.java
@@ -1,6 +1,8 @@
 package com.alisson.gas_station.infrastructure.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -16,10 +18,12 @@ public class FuelPump {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
+    @NotBlank(message = "Fuel Pump name cannot be null")
     private String name;
 
     @ManyToOne
-    @JoinColumn(name = "fuel_type_id")
+    @JoinColumn(name = "fuel_type_id", nullable = false)
+    @NotNull(message = "Fuel Type Id in Fuel Pump cannot be null")
     private FuelType fuelType;
 }
