5000f6a153dc74583ecb5a304abebf7d3cdb2579
Alisson <binho_alisson@hotmail.com>
2025-09-28 01:40:20 -0300

    feat: create validation Fuel Pump Service and moving @Transactional to FuelPumpService Class

--- src/main/java/com/alisson/gas_station/service/FuelPumpService.java
+++ src/main/java/com/alisson/gas_station/service/FuelPumpService.java
@@ -1,38 +1,64 @@
 package com.alisson.gas_station.service;
 
 import com.alisson.gas_station.infrastructure.entities.FuelPump;
+import com.alisson.gas_station.infrastructure.entities.FuelType;
 import com.alisson.gas_station.infrastructure.repositories.FuelPumpRepository;
+import com.alisson.gas_station.infrastructure.repositories.FuelTypeRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class FuelPumpService {
 
     private final FuelPumpRepository fuelPumpRepository;
+    private final FuelTypeRepository fuelTypeRepository;
 
     public void create(FuelPump fuelPump) {
+        Integer fuelTypeId = fuelPump.getFuelType().getId();
+        FuelType fuelType = fuelTypeRepository.findById(fuelTypeId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Fuel Type Id is not found, Id: " + fuelTypeId)
+        );
+
+        fuelPump.setFuelType(fuelType);
+
         fuelPumpRepository.save(fuelPump);
     }
 
     public FuelPump searchFuelPumpById(Integer id) {
-        return fuelPumpRepository
-                .findById(id)
-                .orElseThrow(() -> new NullPointerException("Fuel Pump not found by id" + id));
+        return fuelPumpRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Fuel Pump not found. Id: " + id)
+        );
     }
 
-    @Transactional
     public void deleteFuelPumpById(Integer id) {
+        fuelPumpRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Fuel Pump not found. Id: " + id)
+        );
         fuelPumpRepository.deleteById(id);
     }
 
-    /* todo: validate the data before update the database */
+    public List<FuelPump> getFuelPumps() {
+        return fuelPumpRepository.findAll();
+    }
 
     public void updateFuelPump(Integer id, FuelPump fuelPump) {
-        FuelPump pump = searchFuelPumpById(id);
-        fuelPump.setId(pump.getId());
+        FuelPump existingPump = searchFuelPumpById(id);
 
-        fuelPumpRepository.save(fuelPump);
+        Integer fuelTypeId = fuelPump.getFuelType().getId();
+        FuelType fuelType = fuelTypeRepository.findById(fuelTypeId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Fuel Type Id is not found, Id: " + fuelTypeId)
+        );
+
+        existingPump.setName(fuelPump.getName());
+        existingPump.setFuelType(fuelType);
+
+        fuelPumpRepository.save(existingPump);
     }
 }
